37fe7911d535be4103f19ae401ab39a9a77918df
sasuske <jelver00@hotmail.com>
2023-12-04 08:48:02 -0500

    update changes

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,10 +4,16 @@ import { Link } from 'react-router-dom';
 const Header = () => {
 	return (
 		<>
-			<section>
-				<Link>Quien Soy</Link>
-				<Link>Eventos y Conferencias</Link>
-				<Link>Blog</Link>
+			<section className='section-header'>
+				<div>
+					<Link>Quien Soy</Link>
+				</div>
+				<div>
+					<Link>Eventos y Conferencias</Link>
+				</div>
+				<div>
+					<Link>Blog</Link>
+				</div>
 			</section>
 		</>
 	);
