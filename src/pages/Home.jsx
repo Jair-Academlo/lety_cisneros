2ae373d6298b082d49b239e89fc0d7fb99b50875
sasuske <jelver00@hotmail.com>
2023-12-04 10:27:30 -0500

    update changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import Header from '../components/header/Header';
+import Logos from '../components/logos/Logos';
 import Presentacion from '../components/presentacion/Presentacion';
 
 const Home = () => {
@@ -6,6 +7,7 @@ const Home = () => {
 		<>
 			<Header />
 			<Presentacion />
+			<Logos />
 		</>
 	);
 };
