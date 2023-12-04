d0d407b8f9d0409a1b722ec69c32f6c55a5f9d1d
sasuske <jelver00@hotmail.com>
2023-12-04 08:02:19 -0500

    update changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css';
+import Header from './components/header/Header';
 
 const App = () => {
 	return (
 		<div>
 			<h1>Lety Cisneros</h1>
+			<Header />
 		</div>
 	);
 };
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+import { Container } from 'react-bootstrap';
+import './header.css';
+
+const Header = () => {
+	return (
+		<>
+			<Container>
+				<h1>aqui va el header</h1>
+			</Container>
+		</>
+	);
+};
+
+export default Header;
