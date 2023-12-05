5d75f89a3d50bbcf6f7e879cd207ae7bea4c4ef6
sasuske <jelver00@hotmail.com>
2023-12-05 11:20:02 -0500

    update changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,7 @@ import SobreMi from '../components/sobreMi/SobreMi';
 import Entrevistas from '../components/entrevistas/Entrevistas';
 import Libros from '../components/libros/Libros';
 import Cursos from '../components/cursos/Cursos';
+import ReservarSession from '../components/reservarSession/ReservarSession';
 
 const Home = () => {
 	return (
@@ -18,6 +19,7 @@ const Home = () => {
 				<Entrevistas />
 				<Libros />
 				<Cursos />
+				<ReservarSession />
 			</Container>
 		</>
 	);
