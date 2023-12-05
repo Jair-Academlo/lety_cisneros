383862517b9b2e5c949152159140902754fe2fed
sasuske <jelver00@hotmail.com>
2023-12-05 11:02:17 -0500

    update changes

---
+++ src/components/reservarSession/ReservarSession.jsx
@@ -0,0 +1,11 @@
+import './reservarSession.css';
+
+const ReservarSession = () => {
+	return (
+		<div>
+			<h1>Aqui van las reservas de session</h1>
+		</div>
+	);
+};
+
+export default ReservarSession;
