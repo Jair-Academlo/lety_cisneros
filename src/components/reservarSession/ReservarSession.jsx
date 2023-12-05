73673ec66f1e3a90591e8371657df751a0c85691
sasuske <jelver00@hotmail.com>
2023-12-05 11:33:38 -0500

    update changes

--- src/components/reservarSession/ReservarSession.jsx
+++ src/components/reservarSession/ReservarSession.jsx
@@ -2,9 +2,35 @@ import './reservarSession.css';
 
 const ReservarSession = () => {
 	return (
-		<div>
-			<h1>Aqui van las reservas de session</h1>
-		</div>
+		<>
+			<section className='section-reservar-session'>
+				<div id='container-reservar-session'>
+					<div id='div-session-titulo'>
+						<h1>Titulo para reservar la session</h1>
+					</div>
+					<div id='div-session-descripcion'>
+						Ayudo a Lorem ipsum dolor sit amet consectetur
+						adipisicing elit. Eos animi itaque quae, beatae soluta
+						debitis deleniti sequi aliquid nam similique nesciunt
+						blanditiis, voluptatem odit cumque eligendi
+						necessitatibus fugiat ea eum laudantium? Nemo fuga
+						aliquam dicta reprehenderit iusto. Accusamus doloremque
+						unde quaerat explicabo quis voluptatibus minima dolore,
+						ullam adipisci ab eos.
+					</div>
+					<div id='div-session-button'>
+						<button>Reservar Session</button>
+					</div>
+				</div>
+
+				<div id='div-session-bloque-2'>
+					<img
+						src='https://static.vecteezy.com/system/resources/previews/020/765/399/non_2x/default-profile-account-unknown-icon-black-silhouette-free-vector.jpg'
+						alt='profile'
+					/>
+				</div>
+			</section>
+		</>
 	);
 };
 
