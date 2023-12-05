8824ed4de5582e0a1746b281083a117d48e906c4
sasuske <jelver00@hotmail.com>
2023-12-05 12:03:24 -0500

    update changes

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,31 @@
+import { FaFacebook, FaInstagram, FaYoutube } from 'react-icons/fa';
+
 import './footer.css';
 
 const Footer = () => {
 	return (
-		<div>
-			<h1>Aqui va el footer</h1>
-		</div>
+		<>
+			<section className='section-footer'>
+				<div id='footer-redes-sociales'>
+					<div>
+						<FaFacebook className='footer-redes-sociales-icons' />
+					</div>
+					<div>
+						<FaInstagram className='footer-redes-sociales-icons' />
+					</div>
+					<div>
+						<FaYoutube className='footer-redes-sociales-icons' />
+					</div>
+				</div>
+				<div id='footer-copyrigth'>
+					<h6>
+						Copyright © 2023 Leticia Cisneros Reservados todos los
+						derechos
+					</h6>
+					<h6>Políticas de privacidad</h6>
+				</div>
+			</section>
+		</>
 	);
 };
 
