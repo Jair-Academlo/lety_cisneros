7b4c462abf852954de33fb2daec89d1a3efd623c
sasuske <jelver00@hotmail.com>
2023-12-05 08:36:06 -0500

    update changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import Logos from '../components/logos/Logos';
 import Presentacion from '../components/presentacion/Presentacion';
 import SobreMi from '../components/sobreMi/SobreMi';
 import Entrevistas from '../components/entrevistas/Entrevistas';
+import Libros from '../components/libros/Libros';
 
 const Home = () => {
 	return (
@@ -14,6 +15,7 @@ const Home = () => {
 				<Logos />
 				<SobreMi />
 				<Entrevistas />
+				<Libros />
 			</Container>
 		</>
 	);
