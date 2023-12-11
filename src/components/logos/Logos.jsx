6b8f57fc039c66becbcce50fbbe0159c20274f62
sasuske <jelver00@hotmail.com>
2023-12-11 09:58:59 -0500

    update changes

--- src/components/logos/Logos.jsx
+++ src/components/logos/Logos.jsx
@@ -1,13 +1,13 @@
-import N1 from '../../imgs/N1.png';
-import N2 from '../../imgs/N2.png';
-import N3 from '../../imgs/N3.png';
-import N4 from '../../imgs/N4.png';
-import N5 from '../../imgs/N5.png';
-import N6 from '../../imgs/N6.png';
-import N7 from '../../imgs/N7.png';
-import N8 from '../../imgs/N8.png';
-import N9 from '../../imgs/N9.png';
-import N10 from '../../imgs/N10.png';
+import N1 from '../../imgs/logos/N1.png';
+import N2 from '../../imgs/logos/N2.png';
+import N3 from '../../imgs/logos/N3.png';
+import N4 from '../../imgs/logos/N4.png';
+import N5 from '../../imgs/logos/N5.png';
+import N6 from '../../imgs/logos/N6.png';
+import N7 from '../../imgs/logos/N7.png';
+import N8 from '../../imgs/logos/N8.png';
+import N9 from '../../imgs/logos/N9.png';
+import N10 from '../../imgs/logos/N10.png';
 
 import './logos.css';
 
