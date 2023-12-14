8f81c7ef5e170ea207ebd3ef83fb308c62c07143
sasuske <jelver00@hotmail.com>
2023-12-14 08:21:16 -0500

    update changes

--- src/components/SobreMI/section1/Section1.jsx
+++ src/components/SobreMI/section1/Section1.jsx
@@ -2,9 +2,49 @@ import './section1.css';
 
 const Section1 = () => {
 	return (
-		<div>
-			<h1>esta es la seccion 1 sobre mi</h1>
-		</div>
+		<>
+			<section>
+				<div>
+					<h1>Hola, esta soy yo...</h1>
+					<article>
+						<p>
+							Mexiquense nacida en la ciudad de Toluca, Estado de
+							México, la Dra. <span> Leticia Cisneros </span> es
+							una destacada experta en Derecho con una sólida
+							formación académica y un extenso bagaje de
+							conocimientos. Titulada como Licenciada, Maestra y
+							Doctora en Derecho, ha complementado su formación
+							con estudios de Posdoctorado en la misma disciplina.
+						</p>
+						<br />
+						<p>
+							Posee una vasta trayectoria en el ámbito jurídico,
+							especializándose en áreas clave como Derecho
+							Constitucional, Derecho Administrativo, Derechos
+							Humanos, Juicio de Amparo, y Derecho Electoral,
+							entre otras materias. Además, ha ampliado sus
+							horizontes académicos con la obtención de Maestrías
+							en Psicología Holística y Coaching Personal,
+							demostrando su interés multidisciplinario y su
+							capacidad para integrar diversas perspectivas en su
+							enfoque profesional.
+						</p>
+						<br />
+						<p>
+							Destaca por su compromiso con el desarrollo personal
+							y profesional, habiendo completado numerosos cursos
+							de potencial humano y entrenamientos mentales de
+							alto rendimiento. Su búsqueda constante de la
+							excelencia y la mejora continua la posicionan como
+							una profesional proactiva y orientada a resultados.
+						</p>
+					</article>
+				</div>
+				<div>
+					<p>imagen</p>
+				</div>
+			</section>
+		</>
 	);
 };
 
