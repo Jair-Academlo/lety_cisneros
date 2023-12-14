ca0b94582916249f743fe643cf14df722a475ff7
sasuske <jelver00@hotmail.com>
2023-12-14 10:07:09 -0500

    update changes

--- src/pages/SobreMi.jsx
+++ src/pages/SobreMi.jsx
@@ -1,6 +1,7 @@
 import { Container } from 'react-bootstrap';
 import Section1 from '../components/SobreMI/section1/Section1';
 import Section2 from '../components/SobreMI/section2/Section2';
+import Section3 from '../components/SobreMI/section3/Section3';
 import '../styles/SobreMi.css';
 
 const SobreMi = () => {
@@ -9,6 +10,7 @@ const SobreMi = () => {
 			<Container>
 				<Section1 />
 				<Section2 />
+				<Section3 />
 			</Container>
 		</>
 	);
