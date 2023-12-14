30cb769a21ff71dbea1c4511368f067096e9c8f5
sasuske <jelver00@hotmail.com>
2023-12-14 09:45:29 -0500

    update changes

--- src/components/SobreMI/section2/Section2.jsx
+++ src/components/SobreMI/section2/Section2.jsx
@@ -2,9 +2,33 @@ import './section2.css';
 
 const Section2 = () => {
 	return (
-		<div>
-			<h1>Aqui va la section 2</h1>
-		</div>
+		<>
+			<section className='section-2-acerca-de-mi'>
+				<div className='section-2-acerca-de-mi-div-1'>
+					<img />
+				</div>
+				<div className='section-2-acerca-de-mi-div-2'>
+					<h1>Actividades Literarias</h1>
+					<ul>
+						<li>
+							Autora del best seller &quot;No cambies,
+							transfórmate&quot;, disponible en Amazon desde 2019.
+						</li>
+						<li>
+							Coautora del libro &quot;Mujeres Dreams Boss
+							Abogadas&quot; y colaboradora en diversas
+							publicaciones durante 2021 y 2022.
+						</li>
+						<li>
+							Coordinadora del libro &qout;El Instituto de la
+							Defensoría Pública del Estado de México en el
+							Sexenio del Fortalecimiento al Acceso a la
+							Justicia&quot;, publicado en julio de 2023.
+						</li>
+					</ul>
+				</div>
+			</section>
+		</>
 	);
 };
 
