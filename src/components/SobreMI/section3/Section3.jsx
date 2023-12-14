5d555cb976a5bc97d1ef4137351bd10bc668bc44
sasuske <jelver00@hotmail.com>
2023-12-14 10:16:25 -0500

    update changes

--- src/components/SobreMI/section3/Section3.jsx
+++ src/components/SobreMI/section3/Section3.jsx
@@ -3,102 +3,29 @@ import './section.css';
 const Section3 = () => {
 	return (
 		<>
-			<section className='section-1-acerca-de-mi'>
-				<div className='section-1-acerca-de-mi-div-1'>
-					<h1>Experiencia Profesional Destacada</h1>
-					<div>
-						<h2>Coordinadora de Posgrado</h2>
-						<p>Universidad Autónoma del Estado de México, 2019</p>
-					</div>
-					<div>
-						<h2>
-							Secretaria Particular del Coordinador General de
-							Administración
-						</h2>
-						<p>
-							Oficina de la Presidencia de la República, 2013-2018
-						</p>
-						<ul>
-							<li>
-								Presidente Suplente del Comité de Ética y de
-								Prevención de Conflictos de Interés, 2015-2018.
-							</li>
-						</ul>
-					</div>
-					<div>
-						<h2>Secretaria Particular</h2>
-						<p>
-							Secretaría de Turismo del Estado de México,
-							2012-2013
-						</p>
-					</div>
-					<div>
-						<h2>Presidenta del Consejo Directivo</h2>
-						<p>
-							Colegio de Abogados del Estado de México, A.C.,
-							2013-2015
-						</p>
-					</div>
-					<div>
-						<h2>
-							Subdirectora de Regulación a Empresas de Seguridad
-							Privada
-						</h2>
-						<p>
-							Secretaría de Seguridad Ciudadana del Estado de
-							México, 2011
-						</p>
-					</div>
-					<div>
-						<h2>
-							Subdirectora de Administración y Recursos Materiales
-						</h2>
-						<p>Secretaría General de Gobierno, 2007-2010</p>
-						<ul>
-							<li>
-								Secretaria Técnica del Comité de Adquisiciones y
-								Servicios, 2007-2010.
-							</li>
-						</ul>
-					</div>
-					<div>
-						<h2>Secretaria Técnica</h2>
-						<p>
-							Cámara de Diputados, Comisión de Asuntos
-							Electorales, 2006
-						</p>
-					</div>
-					<div>
-						<h2>
-							Titular del Área de Atención a Medios de Impugnación
-							en Materia Electoral
-						</h2>
-						<p>
-							Instituto Electoral del Estado de México, 2003-2005
-						</p>
-					</div>
-					<div>
-						<h2>Subdirectora de Justicia Electoral</h2>
-						<p>
-							Instituto Electoral del Estado de México, 1998-2003
-						</p>
-					</div>
-					<div>
-						<h2>Jefe de Departamento de Justicia Electoral</h2>
-						<p>
-							Instituto Electoral del Estado de México, 1996-1998
-						</p>
-						<ul>
-							<li>
-								Secretaria Técnica del Comité de Adquisiciones,
-								Arrendamientos, Enajenaciones y Servicios,
-								1996-2003.
-							</li>
-						</ul>
-					</div>
-				</div>
-				<div className='section-1-acerca-de-mi-div-2'>
+			<section className='section-2-acerca-de-mi'>
+				<div className='section-2-acerca-de-mi-div-1'>
 					<img />
+				</div>
+				<div className='section-2-acerca-de-mi-div-2'>
+					<h1>Actividades Literarias</h1>
+					<ul>
+						<li>
+							Autora del best seller &quot;No cambies,
+							transfórmate&quot;, disponible en Amazon desde 2019.
+						</li>
+						<li>
+							Coautora del libro &quot;Mujeres Dreams Boss
+							Abogadas&quot; y colaboradora en diversas
+							publicaciones durante 2021 y 2022.
+						</li>
+						<li>
+							Coordinadora del libro &qout;El Instituto de la
+							Defensoría Pública del Estado de México en el
+							Sexenio del Fortalecimiento al Acceso a la
+							Justicia&quot;, publicado en julio de 2023.
+						</li>
+					</ul>
 				</div>
 			</section>
 		</>
--- src/pages/SobreMi.jsx
+++ src/pages/SobreMi.jsx
@@ -2,6 +2,7 @@ import { Container } from 'react-bootstrap';
 import Section1 from '../components/SobreMI/section1/Section1';
 import Section2 from '../components/SobreMI/section2/Section2';
 import Section3 from '../components/SobreMI/section3/Section3';
+import Section4 from '../components/SobreMI/section4/Section4';
 import '../styles/SobreMi.css';
 
 const SobreMi = () => {
@@ -11,6 +12,7 @@ const SobreMi = () => {
 				<Section1 />
 				<Section2 />
 				<Section3 />
+				<Section4 />
 			</Container>
 		</>
 	);
