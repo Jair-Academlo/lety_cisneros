37cbe449125dc6b7701301fb3e2dc2d08194fab5
sasuske <jelver00@hotmail.com>
2023-12-14 08:57:24 -0500

    update changes

---
+++ src/components/SobreMI/section2/Section2.jsx
@@ -0,0 +1,11 @@
+import './section2.css';
+
+const Section2 = () => {
+	return (
+		<div>
+			<h1>Aqui va la section 2</h1>
+		</div>
+	);
+};
+
+export default Section2;
--- src/pages/SobreMi.jsx
+++ src/pages/SobreMi.jsx
@@ -1,5 +1,6 @@
 import { Container } from 'react-bootstrap';
 import Section1 from '../components/SobreMI/section1/Section1';
+import Section2 from '../components/SobreMI/section2/Section2';
 import '../styles/SobreMi.css';
 
 const SobreMi = () => {
@@ -7,6 +8,7 @@ const SobreMi = () => {
 		<>
 			<Container>
 				<Section1 />
+				<Section2 />
 			</Container>
 		</>
 	);
