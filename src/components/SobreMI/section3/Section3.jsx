e8db90297b9a261984273005588202d5e9bf1d59
sasuske <jelver00@hotmail.com>
2023-12-14 10:47:45 -0500

    update changes

--- src/components/SobreMI/section3/Section3.jsx
+++ src/components/SobreMI/section3/Section3.jsx
@@ -1,31 +1,62 @@
-import './section.css';
+import './section3.css';
 
 const Section3 = () => {
 	return (
 		<>
-			<section className='section-2-acerca-de-mi'>
-				<div className='section-2-acerca-de-mi-div-1'>
-					<img />
+			<section className='section-3-acerca-de-mi'>
+				<div className='section-3-acerca-de-mi-div-1'>
+					<h1>Trascendencia Academica</h1>
+					<div>
+						<h2>Licenciatura en Derecho</h2>
+						<ul>
+							<li>
+								Facultad de Derecho, Universidad Autónoma del
+								Estado de México
+							</li>
+							<li>Cédula Profesional: 2411465</li>
+						</ul>
+					</div>
+					<div>
+						<h2>Maestría en Derecho</h2>
+						<ul>
+							<li>
+								Facultad de Derecho, Universidad Autónoma del
+								Estado de México
+							</li>
+							<li>Cédula Profesional: 9999402</li>
+						</ul>
+					</div>
+					<div>
+						<h2>Doctorado en Derecho</h2>
+						<ul>
+							<li>Escuela de Posgrados en Derecho</li>
+							<li>Cédula Profesional: 12745408</li>
+						</ul>
+					</div>
+					<div>
+						<h2>Posdoctorado en Derecho</h2>
+						<ul>
+							<li>Escuela de Posgrados en Derecho</li>
+						</ul>
+					</div>
+					<div>
+						<h2>Maestría en Psicología Holística</h2>
+						<ul>
+							<li>Grupo INENKA, España</li>
+							<li>Apostillamiento de La Haya</li>
+						</ul>
+					</div>
+					<div>
+						<h2>Maestría en Coaching Personal</h2>
+						<ul>
+							<li>Grupo INENKA, España</li>
+							<li>Apostillamiento de La Haya</li>
+						</ul>
+					</div>
 				</div>
-				<div className='section-2-acerca-de-mi-div-2'>
-					<h1>Actividades Literarias</h1>
-					<ul>
-						<li>
-							Autora del best seller &quot;No cambies,
-							transfórmate&quot;, disponible en Amazon desde 2019.
-						</li>
-						<li>
-							Coautora del libro &quot;Mujeres Dreams Boss
-							Abogadas&quot; y colaboradora en diversas
-							publicaciones durante 2021 y 2022.
-						</li>
-						<li>
-							Coordinadora del libro &qout;El Instituto de la
-							Defensoría Pública del Estado de México en el
-							Sexenio del Fortalecimiento al Acceso a la
-							Justicia&quot;, publicado en julio de 2023.
-						</li>
-					</ul>
+
+				<div className='section-3-acerca-de-mi-div-2'>
+					<img />
 				</div>
 			</section>
 		</>
