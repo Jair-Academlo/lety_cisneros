604a75b714f8cbffad509b35957a2313ff6f39bb
sasuske <jelver00@hotmail.com>
2023-12-21 10:28:53 -0500

    update changes

---
+++ src/components/SobreMI/cargosActuales/CargosActuales.jsx
@@ -0,0 +1,11 @@
+import './cargosActuales.css';
+
+const CargosActuales = () => {
+	return (
+		<div>
+			<h1>aqui van los cargos academicos</h1>
+		</div>
+	);
+};
+
+export default CargosActuales;
