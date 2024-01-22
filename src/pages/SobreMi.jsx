65d14ac8289934fff8317258516b74daf3a48062
sasuske <jelver00@hotmail.com>
2024-01-22 10:13:11 -0500

    update changes

--- src/pages/SobreMi.jsx
+++ src/pages/SobreMi.jsx
@@ -1,4 +1,3 @@
-import CargosActuales from '../components/SobreMI/cargosActuales/CargosActuales';
 import Section1 from '../components/SobreMI/section1/Section1';
 import Section2 from '../components/SobreMI/section2/Section2';
 import Section3 from '../components/SobreMI/section3/Section3';
@@ -10,7 +9,6 @@ const SobreMi = () => {
 		<>
 			<Section1 />
 			<Section2 />
-			<CargosActuales />
 			<Section3 />
 			<Section4 />
 		</>
