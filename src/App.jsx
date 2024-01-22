078048d9d3d2f023dd90294e9d37116a9b92cbbf
sasuske <jelver00@hotmail.com>
2024-01-22 09:31:17 -0500

    update changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import SobreMi from './pages/SobreMi';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
 import Eventos from './pages/Eventos';
+import PoliticaPrivacidad from './pages/PoliticaPrivacidad';
 import './App.css';
 
 const App = () => {
@@ -15,6 +16,7 @@ const App = () => {
 					<Route path='/' element={<Home />} />
 					<Route path='/sobre-mi' element={<SobreMi />} />
 					<Route path='/eventos' element={<Eventos />} />
+					<Route path='/politica' element={<PoliticaPrivacidad />} />
 				</Routes>
 				<Footer />
 			</HashRouter>
